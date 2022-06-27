7dea2266fcf04d5642f6899f49b8f95f34b3f351
金伟强 <jinweiqiang@coding.net>
2022-06-27 16:43:40 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,15 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vitepress/dist
+rm -rf dist
+mv docs/.vitepress/dist .
 
-
-git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:iamjoel/improve-code-quality.git main:gh-pages
+git push
 
 cd -
